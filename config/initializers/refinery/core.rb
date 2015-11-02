242565627100e0eddfb736e62c99bc5d3fdff58f
eliza-irene <elizabeth.korthof@gmail.com>
2015-11-01 19:19:44 -0500

    first commit

---
+++ config/initializers/refinery/core.rb
@@ -0,0 +1,52 @@
+# encoding: utf-8
+Refinery::Core.configure do |config|
+  # When true will rescue all not found errors and display a friendly error page
+  config.rescue_not_found = Rails.env.production?
+
+  # When true this will force SSL redirection in all Refinery backend controllers.
+  # config.force_ssl = false
+
+  # When true will use Amazon's Simple Storage Service instead of
+  # the default file system for storing resources and images
+  config.s3_backend = !(ENV['S3_KEY'].nil? || ENV['S3_SECRET'].nil?)
+
+  # Use a custom Dragonfly storage backend instead of the default
+  # file system for storing resources and images
+  # config.dragonfly_custom_backend_class = nil
+  # config.dragonfly_custom_backend_opts = {}
+
+  # Whenever Refinery caches anything and can set a cache key, it will add
+  # a prefix to the cache key containing the string you set here.
+  # config.base_cache_key = :refinery
+
+  # Site name
+  config.site_name = "Adelina's Bakery"
+
+  # This activates Google Analytics tracking within your website. If this
+  # config is left blank or set to UA-xxxxxx-x then no remote calls to
+  # Google Analytics are made.
+  # config.google_analytics_page_code = "UA-xxxxxx-x"
+
+  # Enable/disable authenticity token on frontend
+  # config.authenticity_token_on_frontend = false
+
+  # Should set this if concerned about DOS attacks. See
+  # http://markevans.github.com/dragonfly/file.Configuration.html#Configuration
+  # config.dragonfly_secret = "a3291967c2e99f4b19929ae231ede58e9ff66d902f948263"
+
+  # Add extra tags to the wymeditor whitelist e.g. = {'tag' => {'attributes' => {'1' => 'href'}}} or just {'tag' => {}}
+  # config.wymeditor_whitelist_tags = {}
+
+  # Register extra javascript for backend
+  config.register_javascript "lightbox"
+  config.register_javascript "gallery"
+
+  # Register extra stylesheet for backend (optional options)
+  config.register_stylesheet "custom", :media => 'screen'
+  config.register_stylesheet "grid", :media => 'screen'
+  config.register_stylesheet "responsive", :media => 'screen'
+  config.register_stylesheet "lightbox", :media => 'screen'
+
+  # Specify a different backend path than the default of /refinery.
+  # config.backend_route = "refinery"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdelinasBakery::Application.config.secret_token = '28095c54178958c9c563a365c257ca4d34fd2347439ac2b5e8ee32cdf62b725b4659dac96047924ed41d1fc86684f95e46e83431f2f6db92d08b475250e74cd9'
